d5d7204924366610baf625c03e7520781e985d61
egools <ergools59@gmail.com>
2019-03-26 13:24:53 -0400

    Enhance BookForm module1

--- src/components/BookForm.vue
+++ src/components/BookForm.vue
@@ -1,8 +1,35 @@
 <template>
-  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class="" action="#" method="post">
-    <input v-model="bookTitle" type="text" name="title" value="" placeholder="Book Title">
-    <input v-model="bookAuthor" type="text" name="author" value="" placeholder="Book Author">
-    <button type="submit" name="button">Add Book</button>
+  <form v-on:submit.prevent="bookSubmit(bookTitle, bookAuthor)" class action="#" method="post">
+    <input v-model="bookData.bookTitle" type="text" name="title" value placeholder="Book Title">
+    <input v-model="bookData.bookAuthor" type="text" name="author" value placeholder="Book Author">
+    <div>
+      <label for="finishedReading">Finished Reading</label>
+      <input
+        type="checkbox"
+        name="finishedReading"
+        id="finishedReading"
+        v-model="bookData.finishedReading"
+      >
+    </div>
+    <div>
+      <label for="borrowed">Borrowed</label>
+      <input
+        type="radio"
+        name="ownership"
+        id="borrowed"
+        value="borrowed"
+        v-model="bookData.ownership"
+      >
+      <label for="bought">Bought</label>
+      <input
+        type="radio"
+        name="ownership"
+        id="bought"
+        value="bought"
+        v-model="bookData.ownership"
+      >
+    </div>
+    <button type="submit" name="button" @submit.prevent="bookSubmit(bookData)">Add Book</button>
   </form>
 </template>
 
@@ -12,13 +39,17 @@ export default {
   props: ["books"],
   data() {
     return {
-      bookTitle: "",
-      bookAuthor: ""
+      bookData: {
+        bookTitle: "",
+        bookAuthor: "",
+        finishedReading: false,
+        ownership: []
+      }
     };
   },
   methods: {
-    bookSubmit(bookTitle, bookAuthor) {
-      this.$emit("addBook", bookTitle, bookAuthor);
+    bookSubmit(bookData) {
+      this.$emit("addBook", bookData);
     }
   }
 };
